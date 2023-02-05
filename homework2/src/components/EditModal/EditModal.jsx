f3e908d89929115cefadd9b971248cbd9eeda2bc
podhorets <podhorets@gmail.com>
2023-02-05 01:30:10 +0100

    refactor homework2

--- homework2/src/components/EditModal/EditModal.jsx
+++ homework2/src/components/EditModal/EditModal.jsx
@@ -2,13 +2,6 @@ import { Component } from "react";
 import "./EditModal.css";
 
 class EditModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: props.title,
-    };
-  }
-
   render() {
     return (
       <div className="modal" onClick={this.props.onClose}>
@@ -21,17 +14,16 @@ class EditModal extends Component {
               rows="4"
               cols="50"
               type="text"
-              value={this.state.title}
-              onChange={(e) => {
-                this.setState({ title: e.target.value });
+              value={this.props.title}
+              onChange={(event) => {
+                this.props.updateTitle(event.target.value);
               }}
             />
           </div>
           <div className="modal-footer">
             <button
               onClick={() => {
-                this.props.editTitle(this.state.title);
-                this.props.hideModal();
+                this.props.editPost(this.props.id);
               }}
               className="button"
             >
